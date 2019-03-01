0794773a96b2b66a2f069369b40d16679deb01c2
Vaibhav Yadav <35480736+Vaibhav911@users.noreply.github.com>
2019-03-01 21:44:55 +0530

    Create course_schema.js

---
+++ course_schema.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://vaibhav:vaibhav@cluster0-txbx7.mongodb.net/test?retryWrites=true"
+);
+
+var Schema = mongoose.Schema;
+
+var fileSchema = new Schema({
+  fileName: { type: String, required: true },
+  fileLink: { type: String, requred: true }
+});
+
+var courseSchema = new Schema({
+  courseId: { type: String, required: true, unique: true },
+  file: [fileSchema]
+});
+
+module.exports = mongoose.model("Course", courseSchema);
